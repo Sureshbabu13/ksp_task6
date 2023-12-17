d3ca7708a149c7304485124689f8d6c1d6c515d8
Suresh babu <147136631+Sureshbabu13@users.noreply.github.com>
2023-12-17 18:38:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("gorijala suresh babu");
+let box1 =2;
+let box2 =3;
+
+console.log(box2);
+
+let number1 =10;
+let number2=20;
+
+console.log(number1+number2);
+// single line
+/* multiple line */
+
+// and &&
+// or ||
+// not !
+
+console.log(number1<20 && number2 <5);
+
+console.log(number1 ==5 || number2 == 4);
+function addition(){
+    console.log(number1+number2);
+
+}
+function substraction(){
+    console.log(number1-number2);
+}
+function C(){
+    console.log("Riding");
+}
+
+addition();
+substraction();
+C();
+
+let score=180;
+ if(score<=180){
+    console.log("good score");
+
+ }
+ else{
+    console.log("badscore");
+ }
